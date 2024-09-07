17dd93717b00c20e5ab6ca4054ab4db5ba8d9c84
AnhLD2610 <leducanh26102002@gmail.com>
2024-09-07 11:53:15 +0700

    first commit

--- model/bert_encoder.py
+++ model/bert_encoder.py
@@ -87,10 +87,10 @@ class Bert_Encoder(base_model):
                 # Concatenate the output tensors and reshape them
                 output1 = torch.cat(output1, dim=0)
                 output1 = output1.view(output1.size(0), -1) # [B,H*2]
-                output2 = torch.cat(output2, dim=0).view(output2[0].size()[0], -1) 
+                output2 = torch.cat(output2, dim=0)
                 output2 = output2.view(output2.size(0), -1) # [B,H*2]
-                print(output1.shape)
-                print(output2.shape)
+                # print(output1.shape)
+                # print(output2.shape)
                 return output1, output2  # Return both outputs for the two concatenated samples
             
             else:
